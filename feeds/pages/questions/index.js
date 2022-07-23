72cc85456b87de74d321d72833962bff29afdcdf
healingfields <dihratene@gmail.com>
2022-07-23 12:26:55 +0100

    enabled SSR For fetching questions

--- feeds/pages/questions/index.js
+++ feeds/pages/questions/index.js
@@ -1,8 +1,7 @@
-import { useEffect, useState } from "react";
-import { useRouter } from 'next/router';
 import styled from "styled-components";
 
 import Card from "../../components/Card";
+import Pagination from "../../components/Pagination";
 
 const QuestionsContainer = styled.div`
 display:flex;
@@ -11,52 +10,44 @@ flex-direction:column;
 margin:5%`;
 
 
-function Questions() {
-    const [loading, setLoading] = useState(true);
-    const [questions, setQuestions] = useState([]);
-
-    const router = useRouter();
-    const { page } = router.query;
-
-
-    useEffect(() => {
-        async function fetchData() {
-            const data = await fetch(
-                `https://api.stackexchange.com/2.3/questions?${page ? `page=${page}&` : ''}order=desc&sort=hot&tagged=reactjs&site=stackoverflow`
-            );
-            const result = await data.json();
-
-            if (result) {
-                setQuestions(result.items);
-                setLoading(false);
-            }
-        }
-        fetchData();
-    }, [page]);
+function Questions({ questions, hasMore, page }) {
 
     return (
         <QuestionsContainer>
             <h2>Questions</h2>
-            {loading ? (<span>Loading ....</span>)
-                : (
-                    <div>
-                        {
-                            questions.map((question) => (
-                                <Card
-                                    key={question.question_id}
-                                    title={question.title}
-                                    views={question.view_count}
-                                    answers={question.answer_count}
-                                />
-                            ))
-                        }
-                    </div>
-                )}
+            <div>
+                {questions.map((question) => (
+                    <Card
+                        key={question.question_id}
+                        title={question.title}
+                        views={question.view_count}
+                        answers={question.answer_count}
+                    />
+                ))}
+            </div>
+            <Pagination currentPage={parseInt(page) || 1} hasMore={hasMore} />
         </QuestionsContainer>
     )
 
+}
 
+export async function getServerSideProps(context) {
 
+    const { page } = context.query;
+
+    const data = await fetch(
+        `https://api.stackexchange.com/2.2/questions?${page ? `page=${page}&` : ''
+        }order=desc&sort=hot&tagged=reactjs&site=stackoverflow`
+    );
+    const result = await data.json();
+
+    return {
+        props: {
+            questions: result.items,
+            hasMore: result.has_more,
+            page: page || 1,
+        }
+    };
 }
 
 export default Questions
